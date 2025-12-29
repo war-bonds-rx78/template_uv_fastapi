fcbd4fe790c5e1d5cfce37cff1584a046b39eba3
war-bonds-rx78 <g.bonds.move.072@gmail.com>
2025-12-30 00:54:30 +0900

    ver0のテンプレート作成

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # template_uv_fastapi
 uv管理作成したfastapiのテンプレート
+
+## フォーマットチェックコマンド
+- black src/ && flake8 src/ && mypy src/
---
+++ src/main.py
@@ -0,0 +1,14 @@
+# src/main.py
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello, uv DevContainer!"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int):
+    return {"item_id": item_id}
